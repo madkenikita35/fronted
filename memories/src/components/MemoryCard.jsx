317ad96bede438831140c6be3e77086a88e11b58
Nikita Satish Madke <madkenikita35@gmail.com>
2026-03-15 11:18:42 +0530

    Add Card Style

--- memories/src/components/MemoryCard.jsx
+++ memories/src/components/MemoryCard.jsx
@@ -88,6 +88,21 @@ const MemoryCard = () => {
           ))}
         </ul>
       </div>
+
+      <div className="memories-Card">
+        {memories.map((mem) => (
+          <div key={mem._id} className="Card-item">
+            <p>{mem.title}</p>
+            <p>{mem.content}</p>
+            <button
+              className="remove-btn"
+              onClick={() => removeMemory(mem._id)}
+            >
+              x
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
