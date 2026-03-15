9cc8187ffb0ca7832f06a4678ff7f0cd537c3a70
Nikita Satish Madke <madkenikita35@gmail.com>
2026-03-15 12:40:02 +0530

    update

--- memories/src/components/CardComponent.jsx
+++ memories/src/components/CardComponent.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+// import React from "react";
 
-const CardComponent = () => {
-  return <div></div>;
-};
+// const CardComponent = () => {
+//   return (
+//     <div>
+//       <div className="memories-Card">
+//         {memories.map((mem) => (
+//           <div key={mem._id} className="Card-item">
+//             <p>{mem.title}</p>
+//             <p>{mem.content}</p>
+//             <button
+//               className="remove-btn"
+//               onClick={() => removeMemory(mem._id)}
+//             >
+//               x
+//             </button>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default CardComponent;
+// export default CardComponent;
---
+++ memories/src/components/ListComponent.jsx
@@ -0,0 +1,27 @@
+// import React from "react";
+// import { useNavigate } from "react-router-dom";
+
+// const ListComponent = () => {
+//   return (
+//     <div>
+//       <div className="memories-mainContainer">
+//         <ul className="memories-container ">
+//           {memories.map((mem) => (
+//             <li key={mem._id}>
+//               <p>{mem.title}</p>
+//               <p>{mem.content}</p>
+//               <button
+//                 className="remove-btn"
+//                 onClick={() => removeMemory(mem._id)}
+//               >
+//                 x
+//               </button>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ListComponent;
