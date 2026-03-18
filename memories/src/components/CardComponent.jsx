b0033f00744a382b6dd757f59f44fae16faca374
Nikita Satish Madke <madkenikita35@gmail.com>
2026-03-18 22:31:47 +0530

    Lifting The State

--- memories/src/components/CardComponent.jsx
+++ memories/src/components/CardComponent.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-const CardComponent = () => {
-  return <div></div>;
+const CardComponent = ({ view }) => {
+  return (
+    <div>
+      {view === "card" && (
+        <div className="memories-Card">
+          {memories.map((mem) => (
+            <div key={mem._id} className="Card-item">
+              <p>{mem.title}</p>
+              <p>{mem.content}</p>
+              <button
+                className="remove-btn"
+                onClick={() => removeMemory(mem._id)}
+              >
+                x
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default CardComponent;
