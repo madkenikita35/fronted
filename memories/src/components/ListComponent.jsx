3b75b519e75154a3ee96cc9a88b7efa07152a792
Nikita Satish Madke <madkenikita35@gmail.com>
2026-03-18 22:50:00 +0530

    Add List Component

--- memories/src/components/ListComponent.jsx
+++ memories/src/components/ListComponent.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ListComponent = ({ view }) => {
+const ListComponent = ({ view, memories, removeMemory }) => {
   return (
     <div>
       {view === "list" && (
