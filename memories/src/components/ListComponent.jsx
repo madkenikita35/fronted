5aa7950123cd82dc3ca84b9505a9826e4ec01535
Nikita Satish Madke <madkenikita35@gmail.com>
2026-03-18 22:31:59 +0530

    Lifting State Up

--- memories/src/components/ListComponent.jsx
+++ memories/src/components/ListComponent.jsx
@@ -1,27 +1,28 @@
-// import React from "react";
-// import { useNavigate } from "react-router-dom";
+import React from "react";
 
-// const ListComponent = () => {
-//   return (
-//     <div>
-//       <div className="memories-mainContainer">
-//         <ul className="memories-container ">
-//           {memories.map((mem) => (
-//             <li key={mem._id}>
-//               <p>{mem.title}</p>
-//               <p>{mem.content}</p>
-//               <button
-//                 className="remove-btn"
-//                 onClick={() => removeMemory(mem._id)}
-//               >
-//                 x
-//               </button>
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// };
+const ListComponent = ({ view }) => {
+  return (
+    <div>
+      {view === "list" && (
+        <div className="memories-mainContainer">
+          <ul className="memories-container ">
+            {memories.map((mem) => (
+              <li key={mem._id}>
+                <p>{mem.title}</p>
+                <p>{mem.content}</p>
+                <button
+                  className="remove-btn"
+                  onClick={() => removeMemory(mem._id)}
+                >
+                  x
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
 
-// export default ListComponent;
+export default ListComponent;
