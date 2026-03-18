820705a92aa3af70bd0573b6a6757d3bf57d5eba
Nikita Satish Madke <madkenikita35@gmail.com>
2026-03-18 22:49:32 +0530

    Add Final App Changes

--- memories/src/App.jsx
+++ memories/src/App.jsx
@@ -20,6 +20,7 @@ import { Routes, Route } from "react-router-dom";
 import CardComponent from "./components/CardComponent";
 import ListComponent from "./components/ListComponent";
 import { useState } from "react";
+import axios from "axios";
 
 const App = () => {
   const [memories, setMemories] = useState([
@@ -28,14 +29,56 @@ const App = () => {
       content: "This is a sample memory content.",
     },
   ]);
+
+  const fetchMemories = async () => {
+    try {
+      const res = await axios.get("http://localhost:3000/memories");
+      setMemories(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const removeMemory = async (id) => {
+    try {
+      await axios.delete(`http://localhost:3000/memories/${id}`);
+      fetchMemories();
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <Routes>
       <Route
         path="/"
-        element={<MemoryCard setMemories={setMemories} memories={memories} />}
+        element={
+          <MemoryCard
+            memories={memories}
+            fetchMemories={fetchMemories}
+            removeMemory={removeMemory}
+          />
+        }
       />
-      <Route path="/list" element={<CardComponent view="list" />} />
-      <Route path="/card" element={<ListComponent view="card" />} />
+      <Route
+        path="/list"
+        element={
+          <ListComponent
+            view="list"
+            memories={memories}
+            removeMemory={removeMemory}
+          />
+        }
+      />
+      <Route
+        path="/card"
+        element={
+          <CardComponent
+            view="card"
+            memories={memories}
+            removeMemory={removeMemory}
+          />
+        }
+      />
     </Routes>
   );
 };
