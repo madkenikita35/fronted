d5ffb09a6d7d82f59fb9d3e60cae0e5b8d353abb
Nikita Satish Madke <madkenikita35@gmail.com>
2026-03-18 22:50:18 +0530

    Add Changes To the Memory Component

--- memories/src/components/MemoryCard.jsx
+++ memories/src/components/MemoryCard.jsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const MemoryCard = ({ memories, setMemories }) => {
+const MemoryCard = ({ memories, fetchMemories, removeMemory }) => {
   const navigate = useNavigate();
   // const [memories, setMemories] = useState([
   //   {
@@ -21,14 +21,14 @@ const MemoryCard = ({ memories, setMemories }) => {
     fetchMemories();
   }, []);
 
-  const fetchMemories = async () => {
-    try {
-      const res = await axios.get("http://localhost:3000/memories");
-      setMemories(res.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  // const fetchMemories = async () => {
+  //   try {
+  //     const res = await axios.get("http://localhost:3000/memories");
+  //     setMemories(res.data);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
 
   const addMemory = async () => {
     if (!newTitle || !newContent) return;
@@ -47,14 +47,14 @@ const MemoryCard = ({ memories, setMemories }) => {
     }
   };
 
-  const removeMemory = async (id) => {
-    try {
-      await axios.delete(`http://localhost:3000/memories/${id}`);
-      fetchMemories();
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  // const removeMemory = async (id) => {
+  //   try {
+  //     await axios.delete(`http://localhost:3000/memories/${id}`);
+  //     fetchMemories();
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
 
   const ChangeCard = () => {
     setCard((prev) => !prev);
