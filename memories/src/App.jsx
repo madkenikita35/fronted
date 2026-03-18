e14392fe74ac3da7c4cd96915c7d5c73fa0fb23c
Nikita Satish Madke <madkenikita35@gmail.com>
2026-03-18 22:23:18 +0530

    Added Changes

--- memories/src/App.jsx
+++ memories/src/App.jsx
@@ -1,3 +1,19 @@
+// import React from "react";
+// import MemoryCard from "./components/MemoryCard";
+// import { Routes, Route } from "react-router-dom";
+
+// const App = () => {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<MemoryCard />} />
+//       <Route path="/list" element={<MemoryCard view="list" />} />
+//       <Route path="/card" element={<MemoryCard view="card" />} />
+//     </Routes>
+//   );
+// };
+
+// export default App;
+
 import React from "react";
 import MemoryCard from "./components/MemoryCard";
 import { Routes, Route } from "react-router-dom";
