cb3a79b08a0cd6b343177c8503778146d66792c5
Nikita Satish Madke <madkenikita35@gmail.com>
2026-03-18 22:31:32 +0530

    Add Changes

--- memories/src/App.jsx
+++ memories/src/App.jsx
@@ -17,13 +17,25 @@
 import React from "react";
 import MemoryCard from "./components/MemoryCard";
 import { Routes, Route } from "react-router-dom";
+import CardComponent from "./components/CardComponent";
+import ListComponent from "./components/ListComponent";
+import { useState } from "react";
 
 const App = () => {
+  const [memories, setMemories] = useState([
+    {
+      title: "Sample Memory",
+      content: "This is a sample memory content.",
+    },
+  ]);
   return (
     <Routes>
-      <Route path="/" element={<MemoryCard />} />
-      <Route path="/list" element={<MemoryCard view="list" />} />
-      <Route path="/card" element={<MemoryCard view="card" />} />
+      <Route
+        path="/"
+        element={<MemoryCard setMemories={setMemories} memories={memories} />}
+      />
+      <Route path="/list" element={<CardComponent view="list" />} />
+      <Route path="/card" element={<ListComponent view="card" />} />
     </Routes>
   );
 };
